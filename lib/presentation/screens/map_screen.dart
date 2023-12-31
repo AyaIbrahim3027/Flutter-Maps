d768e46e8f7a6f07975b24331cf0e490145f8e88
AyaIbrahim3027 <ayaibrahim3027@gmail.com>
2023-12-31 20:05:31 +0200

    Location & Map

--- lib/presentation/screens/map_screen.dart
+++ lib/presentation/screens/map_screen.dart
@@ -1,8 +1,11 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_maps/business_logic/cubit/phone_auth/phone_auth_cubit.dart';
+import 'dart:async';
 
-import '../../constants/strings.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_maps/business_logic/cubit/phone_auth/phone_auth_cubit.dart';
+import 'package:flutter_maps/constants/colors.dart';
+import 'package:flutter_maps/helpers/location_helper.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapScreen extends StatefulWidget {
   const MapScreen({super.key});
@@ -14,34 +17,75 @@ class MapScreen extends StatefulWidget {
 class _MapScreenState extends State<MapScreen> {
   PhoneAuthCubit phoneAuthCubit = PhoneAuthCubit();
 
+  static Position? position;
+  Completer<GoogleMapController> _mapController = Completer();
+
+  static final CameraPosition _myCurrentLocationCameraPosition = CameraPosition(
+    bearing: 0,
+    target: LatLng(position!.latitude, position!.longitude),
+    tilt: 0,
+    zoom: 17,
+  );
+
+  @override
+  void initState() {
+    super.initState();
+    getMyCurrentLocation();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Container(
-          padding: const EdgeInsets.all(20),
-          child: BlocProvider<PhoneAuthCubit>(
-            create: (context) => phoneAuthCubit,
-            child: ElevatedButton(
-              onPressed: () async {
-                await phoneAuthCubit.logout();
-                Navigator.of(context).pushReplacementNamed(loginScreen);
-              },
-              style: ElevatedButton.styleFrom(
-                minimumSize: const Size(110, 50),
-                backgroundColor: Colors.black,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(6),
+      body: Stack(
+        children: [
+          position != null
+              ? buildMap()
+              : Center(
+                  child: CircularProgressIndicator(
+                    color: MyColors.blue,
+                  ),
                 ),
-              ),
-              child: const Text(
-                'Logout',
-                style: TextStyle(color: Colors.white, fontSize: 16),
-              ),
-            ),
+        ],
+      ),
+      floatingActionButton: Container(
+        margin: const EdgeInsets.fromLTRB(0, 0, 8, 30),
+        child: FloatingActionButton(
+          backgroundColor: MyColors.blue,
+          onPressed: _goToMyCurrentLocation,
+          child: const Icon(
+            Icons.place,
+            color: Colors.white,
           ),
         ),
       ),
     );
+  }
+
+  Future<void> getMyCurrentLocation() async {
+    position = await LocationHelper.getCurrentLocation().whenComplete(() {
+      setState(() {});
+    });
+    // position = await Geolocator.getLastKnownPosition().whenComplete(() {
+    //   setState(() {});
+    // });
+  }
+
+  Widget buildMap() {
+    return GoogleMap(
+      mapType: MapType.normal,
+      myLocationEnabled: true,
+      zoomControlsEnabled: false,
+      myLocationButtonEnabled: false,
+      initialCameraPosition: _myCurrentLocationCameraPosition,
+      onMapCreated: (GoogleMapController controller) {
+        _mapController.complete(controller);
+      },
+    );
+  }
+
+  Future<void> _goToMyCurrentLocation() async {
+    final GoogleMapController controller = await _mapController.future;
+    controller.animateCamera(
+        CameraUpdate.newCameraPosition(_myCurrentLocationCameraPosition));
   }
 }
